2f64d701e05af671f60633e3ca18eb99c7df027a
Harsh Patwa <harsh.patwa@systango.com>
2024-11-05 18:27:49 +0530

    unwantend files

--- import.py
+++
@@ -1,7 +0,0 @@
-from export import add, subtract  
-
-result_add = add(5, 3)
-result_subtract = subtract(5, 3)
-
-print(f"Addition Result: {result_add}")      
-print(f"Subtraction Result: {result_subtract}")  
